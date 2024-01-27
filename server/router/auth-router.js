ab1b5c899994c4ef823ca9f4fcf70cc382d2327c
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-27 14:23:01 +0530

    Added ZOD Validation

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express();
 const controller = require('../controller/auth-controller');
+const registerSchema = require('../validators/auth-validator');
+const validate = require('../middleware/validate-middleware');
 
 // router.get('/', (req, res) => {
 //   res
@@ -9,7 +11,10 @@ const controller = require('../controller/auth-controller');
 // });
 
 router.route('/').get(controller.home);
-router.route('/register').post(controller.reg);
+router.route('/register').post(
+    validate(registerSchema),
+    controller.reg
+    );
 router.route('/login').post(controller.login);
 
 module.exports = router;
---
+++ server/validators/auth-validator.js
@@ -0,0 +1,32 @@
+const {z} = require('zod');
+// Zod is a TypeScript-first schema declaration and validation library.
+
+//registerSchema is a schema object that defines the shape of the data that we want to validate.
+const registerSchema = z.object({
+    username: z
+    .string({required_error: "Name is Required"})
+    .trim()
+    .min(3, {message: "Name must be at least 3 characters long"})
+    .max(100, {message: "Name must be at most 100 characters long"}),
+
+    password: z
+    .string({required_error: "Password is Required"})
+    // .trim()
+    .min(6, {message: "Password must be at least 6 characters long"})
+    .max(100, {message: "Password must be at most 100 characters long"}),
+
+    email: z
+    .string({required_error: "Email is Required"})
+    .trim()
+    .email({message: "Invalid Email"})
+    .min(6, {message: "Email must be at least 6 characters long"})
+    .max(100, {message: "Email must be at most 100 characters long"}),
+
+    phone: z
+    .string({required_error: "Phone is Required"})
+    .trim()
+    .min(10, {message: "Phone must be at least 10 characters long"})
+    .max(10, {message: "Phone must be at most 10 characters long"}),
+});
+
+module.exports = registerSchema;
