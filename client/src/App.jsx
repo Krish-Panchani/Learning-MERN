7df727196b8709d4db8f04b46b008b01cc506bd1
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-29 23:18:27 +0530

    Add react-router-dom and implement basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
 const App = () => {
   return (
-    <div>
-      <h1>React App</h1>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+      <Routes>
+       <Route exact path="/" element ={ <Home /> } />
+       </Routes>
+      </BrowserRouter>
+    </>
+      );
 }
 
 export default App;
