29fd33cd3b0aace5dbff523e0941ebf7ef2f9f73
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-29 23:55:32 +0530

    Add Navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+
+        <>
+        <header>
+            <div className="container">
+                <div className="logo-brand">
+                    <NavLink to="/">Krish</NavLink>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li> <NavLink to="/">Home</NavLink> </li>
+                        <li> <NavLink to="/service">Service</NavLink> </li>
+                        <li> <NavLink to="/about">About</NavLink> </li>
+                        <li> <NavLink to="/contact">Contact</NavLink> </li>
+                        <li> <NavLink to="/login">login</NavLink> </li>
+                        <li> <NavLink to="/register">register</NavLink> </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        </>
+    )
+}
