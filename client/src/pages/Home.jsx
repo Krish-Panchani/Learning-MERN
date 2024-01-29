58aaac3ca7b1b5c0e60483caf11d9df4ac0e9f2c
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-29 23:19:28 +0530

    Refactor Home component to use arrow function syntax

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,7 @@
-export default function Home() {
-    return(
-        <div>Home Page</div>
-    )
+export const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+        </>
+    );
 }
