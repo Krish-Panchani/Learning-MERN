b9cb1e4eb5899db108bde5c74608e286e5b6650a
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-29 23:55:27 +0530

    Add Navbar component to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
 import { Service } from './pages/Service';
 import { Register } from './pages/Register';
 import { Login } from './pages/Login';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
