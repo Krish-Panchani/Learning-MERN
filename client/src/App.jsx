b9bd59a939c85a137312331fd8663a6b817edb94
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-29 23:28:10 +0530

    Add routes for About, Contact, Service, Register, and Login pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { Service } from './pages/Service';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <Routes>
-       <Route exact path="/" element ={ <Home /> } />
-       </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/service" element={<Service />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </>
-      );
+  );
 }
 
 export default App;
